4e9480366935a9fbc22258d69ce25994a8f6943d
Perera M.P.P <99342362+IT21214066@users.noreply.github.com>
2022-05-19 10:21:25 +0000

    Added the header file of Admin

---
+++ Account.cpp
@@ -0,0 +1,28 @@
+#include "Account.h"
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+Account::Account() {
+strcpy(AccountID, "");
+strcpy(AccountType, "");
+}
+
+Account::Account(char ID[]) {
+strcpy(AccountID, ID);
+
+}
+
+void Account::setAccounttype(char type) {
+
+};
+void Account:: DisplayAccount() {
+p1[1] = new Payment();
+}
+Account::~Account() { //Destructor
+for (int i = 0; i < 5; i++)
+  {
+  delete p1[i];
+  }
+ 
+};
---
+++ Admin.h
@@ -0,0 +1,17 @@
+#include "Payment.h"
+#include "Report.h"
+#include "Account.h"
+
+class Admin {
+protected:
+char adminID[5];
+char AdminName[20];
+Report* r1[4];
+
+public:
+Admin();
+Admin(char ID[], const char name[], Account*Ac1, Payment*p1);
+void displayAdmin(); 
+
+~Admin();
+};
